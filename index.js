5f02b9469039923f0a505ccde14fa9460af87ad4
tstariqul <tariqul15-2250@diu.edu.bd>
2021-03-30 11:41:00 +0600

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
+const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// alert("hello world")
+let js = "amazing";
+if (js === "amazing") console.log("Fun");
+js = "boring";
+if (js === "boring") console.log("see");
