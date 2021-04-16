3fac5264145abf3c5efa92ab5ac9d65b2ed144be
tstariqul <tariqul15-2250@diu.edu.bd>
2021-04-16 11:54:57 +0600

    1 file modified

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-// Import stylesheets
-//using strict
+'using strict'
 import "./style.css";
 
-// Write Javascript code!
-const appDiv = document.getElementById("app");
-appDiv.innerHTML = `<h1>Everything Starter</h1>`;
-// alert("hello world");
-let js = "amazing";
-if (js === "amazing") console.log("Fun and 'make' bueaty");
-js = "boring";
-if (js === "boring") console.log(10 + 2 + 1);
-console.log(typeof true);
-
-var now = 2037;
-var ageMom = now - 1900;
-var ageTariqul = now - 1990;
-console.log(ageMom, ageTariqul);
-//templete string
-console.log(ageMom + " " + ageTariqul);
-console.log(2 ** 3);
-
-const year = [2020, 2021, 2011];
-const name = new Array("tar", "nam", "abul");
-console.log(name);
